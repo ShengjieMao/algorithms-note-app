f10db7c44982582dc6878bf28ec38e410240eb99
ShengjieMao <56333697+ShengjieMao@users.noreply.github.com>
2023-04-29 03:03:56 -0700

    Frame of the app

---
+++ data.js
@@ -0,0 +1,46 @@
+const CATEGORIES = [
+  { name: "Array and String", color: "#3b82f6" },
+  { name: "Tree and Linked-list", color: "#16a34a" },
+  { name: "Hash table", color: "#ef4444" },
+  { name: "Heap and Graph", color: "#eab308" },
+  { name: "Queue and Stack", color: "#db2777" },
+  { name: "Binary", color: "#14b8a6" },
+  { name: "Dynamic programming", color: "#f97316" },
+  { name: "Recursion", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "Sliding window technique can apply to questions related to subarray",
+    source:
+      "https://discuss.leetcode.com/topic/30941/here-is-a-10-line-template-that-can-solve-most-substring-problems",
+    category: "Array and String",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2023,
+  },
+  {
+    id: 2,
+    text: "Hash takes fewer time to search for the target but requires more space",
+    source:
+      "https://www.coursera.org/lecture/data-structures-optimizing-performance/core-hash-tables-m7UuP",
+    category: "Hash table",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2023,
+  },
+  {
+    id: 3,
+    text: "Sometimes we can only store the last two values or rows if the matrix in memory",
+    source:
+      "http://www.cs.yale.edu/homes/aspnes/classes/223/notes.html#dynamicProgramming",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2023,
+  },
+];
